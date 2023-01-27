5f361aff427771484a3308edc5f8750970d0fd7e
pablomt90 <pablomt90@gmail.com>
2023-01-27 19:17:11 +0100

    Ejercicio3

---
+++ Ejercicio3/index.js
@@ -0,0 +1,11 @@
+const data = ["hola", 2, 5, "adios"];
+
+//Determinar número mayor
+console.log(Math.max(data[1],data[2]));
+
+//Realizar operaciones numérica
+console.log(data[1] + data[2]);
+console.log(data[1] - data[2]);
+console.log(data[1] * data[2]);
+console.log(data[1] / data[2]);
+console.log(data[1] % data[2]);
