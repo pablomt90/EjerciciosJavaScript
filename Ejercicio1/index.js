a92e59a5977d9a991c510e7856246f2bee1a7979
pablomt90 <pablomt90@gmail.com>
2023-01-27 18:47:47 +0100

    Ejercicio1

---
+++ Ejercicio1/index.js
@@ -0,0 +1,12 @@
+console.log('hola');
+// base 16 * 23 cm
+// altura 13 cm
+
+const volumen = 16 * 23 *13 
+console.log('volumen', volumen + 'cm3');
+
+// A = al + 2ab
+const areaLateral = 23 * 13; 
+const areaBase = 16 * 23;
+const areaTotal = areaLateral + 2 * areaBase;
+console.log('areaTotal', areaTotal);
