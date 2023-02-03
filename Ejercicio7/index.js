29a841f08d372390a411ae4140ade5d2d16ef2b6
pablomt90 <pablomt90@gmail.com>
2023-02-03 18:28:53 +0100

    Ejercicio7

--- Ejercicio7/index.js
+++ Ejercicio7/index.js
@@ -1,11 +1,14 @@
-const num = [1,2,3,4,5,6];
+function NumerosPares(){
+    const numeritos = prompt("Ingrese una serie de números separados por coma", "Ej: 1,5,8,78,64").split(',');
+    const sum = 0;
+    for (const i =0; i < numeritos.length; i++){
+        if (numeritos[i]% 2 === 0){
+            sum += parseInt(numeritos[i])
+        }
+    }
+console.log(sum);
+}
 
-
-function sumanumeros(num){
-    let resultado = num[1]+num[3];
-    return resultado
-    console.log ("resultado", resultado);
-};
-
+NumerosPares()
 
 
